47f149e1cab59d17533b904f2d308515a488d865
turningbookcht <35246267+turningbookcht@users.noreply.github.com>
2018-01-09 11:36:34 +0800

    get line message

--- receive.php
+++ receive.php
@@ -1,3 +1,9 @@
 <?php
-  echo "Hi I'm turningbookcht"
+  $json_str = file_get_contents('php://input');   //接收REQUEST的BODY
+  $json_obj = json_decode($json_str);   //轉JSON格式
+
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!");   //設定一個log.txt 用來印訊息
+  fwrite($myfile, "\xEF\xBB\xBf".$json_str);   //在字串前加入\xEF\xBB\xBf轉成utf8格式
+  fclose($myfile);
+  
 ?>
